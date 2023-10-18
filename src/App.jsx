a281c9df7a4b1c2247e857d74c8805f0341cb316
vectorell <vlindell@outlook.com>
2023-10-18 15:57:31 +0200

    Added first framer-element

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import "./App.css";
 import MoviesByLength from "./components/MoviesByLength/MoviesByLength.jsx";
 import MoviesByGenre from "./components/MoviesByGenre/MoviesByGenre.jsx";
 import Search from "./components/Seach/Search.jsx";
+import { motion } from 'framer-motion';
+import { useState } from "react";
 
 
 
 function App() {
+  const [showProducedInLanguage, setShowProducedInLanguage] = useState(false)
 
+  function handleClick() {
+    setShowProducedInLanguage(!showProducedInLanguage)
+    console.log(showProducedInLanguage);
+  }
 
 
 
@@ -17,11 +24,28 @@ function App() {
 
           <h1> MovieTown </h1>
 
-          {/* <ProducedInLanguage /> */}
+          <motion.div
+            className="container"
+            initial={{ scale: 0 }}
+            animate={{ rotate: 360, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 20
+            }}
+            whileHover={{ 
+              scale: 1.05, 
+              cursor: 'pointer',
+            }}
+            whileTap={{ scale: 0.95 }}
+            onClick={() => handleClick()}
+          > Show amount of movies per language </motion.div>
+
+          {showProducedInLanguage && <ProducedInLanguage />}
           {/* <MoviesByPremiere /> */}
           {/* <MoviesByLength /> */}
           {/* <MoviesByGenre /> */}
-          <Search />
+          {/* <Search /> */}
 
         </div>
     );
